6cb4b9093157e29b325f9bffb319df986468c7e9
Tiemi Faustino <hayashipharma@gmail.com>
2022-06-11 11:00:53 -0300

    :construction: Requisito 1 em andamento

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
+const talkers = require('./talker.json');
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', (_req, res) => {
+  res.status(HTTP_OK_STATUS).json(talkers);
 });
 
 app.listen(PORT, () => {
