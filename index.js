4c8c68b7bf3e9b7045bd0841e45cef3bcb9c36d7
Tiemi Faustino <hayashipharma@gmail.com>
2022-06-21 16:24:46 -0300

    :recycle: refactor: instalação e uso do pacote express-rescue-tratamento de erros

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rescue = require('express-rescue');
 const bodyParser = require('body-parser');
 const getTalker = require('./middlewares/getTalker');
 const getAllTalkers = require('./middlewares/getAllTalkers');
@@ -27,39 +28,39 @@ app.get('/', (_request, response) => {
 });
 
 // Req 1
-app.get('/talker', getAllTalkers);
+app.get('/talker', rescue(getAllTalkers));
 
 // Req 8
-app.get('/talker/search', validateToken, validateQuery, findTalkerName);
+app.get('/talker/search', rescue(validateToken), rescue(validateQuery), rescue(findTalkerName));
 
 // Req 2
-app.get('/talker/:id', getTalker);
+app.get('/talker/:id', rescue(getTalker));
 
 // Req 3 e Req 4
-app.post('/login', validateEmail, validatePassword, postToken);
+app.post('/login', rescue(validateEmail), rescue(validatePassword), rescue(postToken));
 
 // Middleware utilizado para Req 5, 6 e 7 
 app.use(validateToken);
 
 // Req 5
 app.post('/talker',
-  validateName,
-  validateAge,
-  validateTalk,
-  validateWatchedAt,
-  validateRate,
-  createTalker);
+  rescue(validateName),
+  rescue(validateAge),
+  rescue(validateTalk),
+  rescue(validateWatchedAt),
+  rescue(validateRate),
+  rescue(createTalker));
 
 // Req 6
 app.put('/talker/:id',
-  validateName,
-  validateAge,
-  validateTalk,
-  validateWatchedAt,
-  validateRate,
-  updateTalker);
+  rescue(validateName),
+  rescue(validateAge),
+  rescue(validateTalk),
+  rescue(validateWatchedAt),
+  rescue(validateRate),
+  rescue(updateTalker));
 
 // Req 7
-app.delete('/talker/:id', deleteTalker);
+app.delete('/talker/:id', rescue(deleteTalker));
 
 app.listen(PORT, () => console.log('Online'));
