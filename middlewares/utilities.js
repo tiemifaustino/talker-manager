5d567cec87c4a943ed52c07a3ea85bfff7a619c1
Tiemi Faustino <hayashipharma@gmail.com>
2022-06-21 11:53:59 -0300

    :recycle: refactor: organização middlewares

--- middlewares/utilities.js
+++ middlewares/utilities.js
@@ -19,7 +19,7 @@ const deleteContentFile = async (req, res) => {
 const validateQuery = async (req, res, next) => {
   const { q } = req.query;
   const talkers = await readContentFile();
-  if (!q || q === '') return res.status(200).json(JSON.parse(talkers));
+  if (!q || q === '') return res.status(200).json(talkers);
   next();
 };
 
